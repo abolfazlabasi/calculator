b61dd4e9552d4dd5d145ff2bdbcb5acfa3f6929b
Abolfazlabasi <83183132+abolfazlabasi@users.noreply.github.com>
2022-05-25 13:58:51 +0430

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var number = "0";
+var result_text = document.getElementById("res-text");
+var is_decimal = false;
+var equation = 0, last_event = "";
+
+function update_show(text){
+    result_text.innerText = text;
+}
