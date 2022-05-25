d6f4dc9df44c022506851a99b0320cadddbe3a95
Abolfazlabasi <83183132+abolfazlabasi@users.noreply.github.com>
2022-05-25 13:59:57 +0430

    script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ var equation = 0, last_event = "";
 function update_show(text){
     result_text.innerText = text;
 }
+function add_digit(digit){
+    if (number != "0" && number != "NaN" && number != "Cannot divide by zero") number += digit;
+    else{
+        if (digit != ".")
+            number = digit;
+        else
+            number = "0.";
+    }
